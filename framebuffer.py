9a81262e97f2d0db4407ad1d3112514ff1a7d224
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-01-23 13:53:11 +0000

    using classes for structs

--- framebuffer.py
+++ framebuffer.py
@@ -8,39 +8,72 @@ from collections import namedtuple
 FBIOGET_VSCREENINFO = 0x4600
 FBIOGET_FSCREENINFO = 0x4602
 
+
+class Finfo_struct:
+    def __init__(self, args):
+        self.id = args[1]
+        self.smem_start = args[2]
+        self.smem_len = args[3]
+        self.type = args[4]
+        self.type_aux = args[5]
+        self.visual = args[6]
+        self.xpanstep = args[7]
+        self.ypanstep = args[8]
+        self.ywrapstep = args[9]
+        self.line_length = args[10]
+        self.mmio_start = args[11]
+        self.mmio_len = args[12]
+        self.accel = args[13]
+        self.capabilities = args[14]
+        self.reserved = args[15:]
+
+
+class Fb_bitfield:
+    def __init__(self, offset, length, msb_right):
+        self.offset = offset
+        self.length = length
+        self.msb_right = msb_right
+
+
+class Vinfo_struct:
+    def __init__(self, args):
+        self.xres = args[0]
+        self.yres = args[1]
+        self.xres_virtual = args[2]
+        self.yres_virtual = args[3]
+        self.xoffset = args[4]
+        self.yoffset = args[5]
+        self.bits_per_pixel = args[6]
+        self.grayscale = args[7]
+        self.red = Fb_bitfield(args[8], args[9], args[10])
+        self.green = Fb_bitfield(args[11], args[12], args[13])
+        self.blue = Fb_bitfield(args[14], args[15], args[16])
+        self.transp = Fb_bitfield(args[17], args[18], args[19])
+        self.nonstd = args[20]
+        self.activate = args[21]
+        self.height = args[22]
+        self.width = args[23]
+        self.accel_flags = args[24]
+        self.pixclock = args[25]
+        self.left_margin = args[26]
+        self.right_margin = args[27]
+        self.upper_margin = args[28]
+        self.lower_margin = args[29]
+        self.hsync_len = args[30]
+        self.vsync_len = args[31]
+        self.sync = args[32]
+        self.vmode = args[33]
+        self.rotate = args[34]
+        self.colorspace = args[35]
+        self.reserved = args[36:]
+
+
 class Framebuffer():
     '''Framebuffer device'''
     def __init__(self):
         self.dev = 0
         self.finfo = 0
-        self.vinfo = namedtuple('vinfo', '''
-xres yres
-xres_virtual yres_virtual
-xoffset yoffset
-bits_per_pixel
-grayscale
-red_offset red_length red_msb_right
-green_offset green_length green_msb_right
-blue_offset blue_length blue_msb_right
-transp_offset transp_length transp_msb_right
-nonstd
-activate
-height
-width
-accel_flags
-pixclock
-left_margin
-right_margin
-upper_margin
-lower_margin
-hsync_len
-vsync_len
-sync
-vmode
-rotate
-colorspace
-reserved_1 reserved_2 reserved_3 reserved_4
-''')
+        self.vinfo = 0
 
     def open(self, dev_name):
         try:
@@ -52,16 +85,21 @@ reserved_1 reserved_2 reserved_3 reserved_4
     def get_fb_info(self):
         # TODO clean this up and error check
         fix_fmt = "<16sL4I3HIL2I2H"
-        fix_buf = struct.pack(fix_fmt, bytes(0),0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        junk_buf = [bytes(0)] + [0 for i in range(14)]
+        fix_buf = struct.pack(fix_fmt, *junk_buf)
         fb_fix_screen_info = fcntl.ioctl(self.dev, FBIOGET_FSCREENINFO, fix_buf, True)
-        print(struct.unpack(fix_fmt, fb_fix_screen_info))
+        self.finfo = Finfo_struct(struct.unpack_from(fix_fmt, fb_fix_screen_info))
+        print(self.finfo.line_length)
 
         var_fmt = "8I3I3I3I3I16I4I"
-        var_buf = struct.pack(var_fmt, 0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,
-                                         0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0,0)
+        junk_buf = [0 for i in range(40)]
+        var_buf = struct.pack(var_fmt, *junk_buf)
         fb_var_screen_info = fcntl.ioctl(self.dev, FBIOGET_VSCREENINFO, var_buf, True)
-        print(struct.unpack(var_fmt, fb_var_screen_info))
-        print(self.vinfo._make(struct.unpack(var_fmt, fb_var_screen_info)))
+        self.vinfo = Vinfo_struct(struct.unpack_from(var_fmt, fb_var_screen_info))
+        print(self.vinfo.xres)
+
+    def test_fill(self):
+        pass
 
     def write(self):
         self.dev.seek(0)
@@ -76,6 +114,7 @@ def main():
     f = Framebuffer()
     f.open('/dev/fb0')
     f.get_fb_info()
+    f.test_fill()
     f.close()
 
 
