fef2716528e0cb0b38298626d8b82cb3489312e6
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-01-23 11:27:29 +0000

    getting fcntl to work

--- framebuffer.py
+++ framebuffer.py
@@ -1,27 +1,44 @@
 #!/usr/bin/env python3
 
 import sys
+import fcntl
+import array
+
+FBIOGET_VSCREENINFO = 0x4600
+FBIOGET_FSCREENINFO = 0x4602
 
 
 class Framebuffer():
     '''Framebuffer device'''
     def __init__(self):
-        self.fb = 0
+        self.dev = 0
+        self.finfo = 0
+        self.vinfo = 0
 
     def open(self, dev_name):
         try:
-            self.fb = open(dev_name, 'r+')
+            self.dev = open(dev_name, 'r+')
         except FileNotFoundError:
             print("Error: " + dev_name + " not found!")
             sys.exit(-1)
 
+    def get_fb_info(self):
+        buf = array.array('h', [0])
+        fcntl.ioctl(self.dev, FBIOGET_FSCREENINFO, buf, 1)
+
+    def write(self):
+        self.dev.seek(0)
+        self.dev.write(self.dev)
+        self.dev.truncate()
+
     def close(self):
-        self.fb.close()
+        self.dev.close()
 
 
 def main():
     f = Framebuffer()
     f.open('/dev/fb0')
+    f.get_fb_info()
     f.close()
 
 
