ea7c028410bf6c6af64cf886371b824d5199a3b4
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-01-23 10:45:28 +0000

    open fb dev

--- framebuffer.py
+++ framebuffer.py
@@ -1,4 +1,18 @@
 #!/usr/bin/env python3
 
+
+class Framebuffer():
+    def __init__(self):
+        self.fb = 0
+
+    def open(self, dev_name):
+        self.fb = open(dev_name, 'r+')
+
+    def close(self):
+        self.fb.close()
+
+
 if __name__ == '__main__':
-    pass
+    f = Framebuffer()
+    f.open('/dev/fb0')
+    f.close()
