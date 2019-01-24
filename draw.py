7e39e16e6cb31f60a40051052b56977ca7ccd8d0
F.Fitzgerald <mrffitzgerald@runbox.com>
2019-01-24 19:12:00 +0000

    draw random lines example

--- draw.py
+++ draw.py
@@ -4,6 +4,7 @@ import framebuffer
 import struct
 import sys
 import time
+import random
 
 # TODO: see if transparency works, probably not
 
@@ -27,7 +28,7 @@ def draw_line(f, x1, y1, x2, y2, col):
     if dx <= dy: err = -dy/2
 
     while 1:
-        f.colour_pixels(pix_location(f, x1, y1), 10, col)
+        f.colour_pixels(pix_location(f, x1, y1), 1, col)
         if x1 == x2 and y1 == y2: return
         e2 = err
         if e2 > -dx :
@@ -37,26 +38,40 @@ def draw_line(f, x1, y1, x2, y2, col):
             err += dx
             y1 += sy
 
+def draw_random_lines(f):
+    for i in range(30):
+        col = pix_colour(random.randint(0, 255), 
+                random.randint(0, 255), random.randint(0, 255),
+                f.vinfo)
+        draw_line(f,
+                random.randint(0, f.vinfo.xres),
+                random.randint(0, f.vinfo.yres),
+                random.randint(0, f.vinfo.xres),
+                random.randint(0, f.vinfo.yres),
+                col)
+
 def main():
     f = framebuffer.Framebuffer()
     f.open()
     f.setup()
     # print(f.finfo.__dict__)
     # print(f.vinfo.__dict__)
-    f.vinfo.xres = 640
-    f.vinfo.yres = 480
+    f.vinfo.xres = 800
+    f.vinfo.yres = 600
     f.vinfo.grayscale = 0
     f.vinfo.bits_per_pixel = 32
     f.put_vinfo()
 
     f.clear()
 
-    draw_line(f, 0, 0, f.vinfo.xres, f.vinfo.yres, pix_colour(255, 255, 255, f.vinfo))
+    #draw_line(f, 0, 0, f.vinfo.xres, f.vinfo.yres, pix_colour(255, 255, 255, f.vinfo))
 
     #for y in range(f.vinfo.yres):
         #location = (y + f.vinfo.yoffset) * f.finfo.line_length
         #col = struct.pack('I', pix_colour(y % 255, 255, 255, f.vinfo))
         #f.colour_pixels(location, f.vinfo.xres, col)
+
+    draw_random_lines(f)
     time.sleep(1)
 
     # since I set vinfo better restore it now
